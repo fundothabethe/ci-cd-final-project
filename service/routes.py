fd18f364166ab05cb997d85382a9a0eea434e543
fundondoh <fundondoh@gmail.com>
2024-05-24 04:06:26 -0400

    Done

--- service/routes.py
+++ service/routes.py
@@ -40,8 +40,10 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
-
+    counters = [
+        dict(name=count[0], counter=count[1]) 
+        for count in COUNTER.items()
+    ]
     return jsonify(counters)
 
 
@@ -54,7 +56,10 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        return abort(
+            status.HTTP_409_CONFLICT, 
+            f"Counter {name} already exists"
+        )
 
     COUNTER[name] = 0
 
@@ -72,14 +77,21 @@ def create_counters(name):
 @app.route("/counters/<name>", methods=["GET"])
 def read_counters(name):
     """Reads a single counter"""
-    app.logger.info("Request to Read counter: %s...", name)
+    app
+    .logger
+    .info(
+        "Request to Read counter: %s...", 
+        name
+    )
 
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
 
     counter = COUNTER[name]
-    return jsonify(name=name, counter=counter)
-
+    return jsonify(
+        name=name,
+        counter=counter
+    )
 
 ############################################################
 # Update counters
